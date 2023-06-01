92ee737a2add425a2c9608d3383fcc0c25d6c0e6
glqdlt <dlfdnd0725@gmail.com>
2023-06-01 12:12:27 +0900

    code polishing

--- src/test/java/com/github/glqdlt/sqlgenerator/SelectQeuryTest.java
+++ src/test/java/com/github/glqdlt/sqlgenerator/SelectQeuryTest.java
@@ -25,10 +25,10 @@ public class SelectQeuryTest {
     public void canMakeSelectQuery3() {
         SimpleSelectQueryGenerator query
                 = new SimpleSelectQueryGenerator("table_abc");
-        query.command("count(*)");
-        query.order("order by column1 desc");
-        query.criteria("column1 = 'a'", "column2 = 'b'");
-        query.offset("limit 10,100");
+        query.command("count(*)")
+                .order("order by column1 desc")
+                .criteria("column1 = 'a'", "column2 = 'b'")
+                .offset("limit 10,100");
         Assert.assertEquals("select count(*) from table_abc where column1 = 'a' and column2 = 'b' order by column1 desc limit 10,100", query.generate());
     }
 }
